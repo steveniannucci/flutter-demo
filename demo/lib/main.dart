ec362d7a6ac8bda8e67f51a1ab07233d092996c8
StevenIannucci <89707282+StevenIannucci@users.noreply.github.com>
2024-04-14 03:19:53 +0000

    Apr 13, 2024, 11:19 PM

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -11,7 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text('Demo App'),
+        ),
       ),
     );
   }
