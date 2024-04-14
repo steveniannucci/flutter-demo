2ae3153f0d62ad921f9102b5a3b13fd3ffec1ac8
StevenIannucci <89707282+StevenIannucci@users.noreply.github.com>
2024-04-14 18:00:11 +0000

    Apr 14, 2024, 2:00 PM

--- demo/test/widget_test.dart
+++ demo/test/widget_test.dart
@@ -11,20 +11,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:demo/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Adding task test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
+    // Tap the '+' icon to open the dialog.
     await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.pumpAndSettle();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Enter text into the TextField.
+    await tester.enterText(find.byType(TextField), 'Test task');
+
+    // Tap the 'Add' button and trigger a frame.
+    await tester.tap(find.widgetWithText(TextButton, 'Add'));
+    await tester.pumpAndSettle();
+
+    // Verify that our task has been added.
+    expect(find.text('Test task'), findsOneWidget);
   });
 }
