a9f4eb040b9c1bfa16047aded77f7e0bab0bde1b
StevenIannucci <89707282+StevenIannucci@users.noreply.github.com>
2024-05-03 02:08:26 +0000

    May 2, 2024, 10:08 PM

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,104 +1,46 @@
-// ignore_for_file: prefer_const_constructors
-
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'To-Do App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
       ),
-      home: TodoListScreen(),
     );
   }
 }
 
-class TodoListScreen extends StatefulWidget {
-  @override
-  _TodoListScreenState createState() => _TodoListScreenState();
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
 }
 
-class _TodoListScreenState extends State<TodoListScreen> {
-  List<String> todos = [];
-
-  TextEditingController _textEditingController = TextEditingController();
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'To-Do List',
-          style: TextStyle(
-            color: Colors.white,
-            ),
-          ),
-        backgroundColor:  const Color.fromARGB(255, 0, 0, 0),
+      body: Column(
+        children: [
+          Text('A random idea:'),
+          Text(appState.current.asLowerCase),
+        ],
       ),
-      body: 
-      ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            decoration: BoxDecoration(
-              color: Colors.black,
-              border: Border.all(color: Colors.black),
-            ),
-            margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-            child: ListTile(
-              title: Text(
-                todos[index],
-                style: TextStyle(color: Colors.white),
-                ),
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _addTodoDialog(context);
-        },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-
-  void _addTodoDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Add Todo'),
-          content: TextField(
-            controller: _textEditingController,
-            decoration: InputDecoration(hintText: 'Enter your task'),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: Text('Add'),
-              onPressed: () {
-                setState(() {
-                  todos.add(_textEditingController.text);
-                  _textEditingController.clear();
-                });
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
     );
   }
 }
