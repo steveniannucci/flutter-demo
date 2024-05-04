0c023023454221cfcc24e6f01c3e11c8829db4d3
StevenIannucci <89707282+StevenIannucci@users.noreply.github.com>
2024-05-04 16:36:53 +0000

    May 4, 2024, 12:36 PM

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -58,11 +58,22 @@ class MyHomePage extends StatelessWidget {
           children: [
             BigCard(pair: pair),
             SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                appState.getNext();
-              },
-              child: Text('Next'),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  child: Text('Like'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  },
+                  child: Text('Next'),
+                ),
+              ],
             ),
           ],
         ),
