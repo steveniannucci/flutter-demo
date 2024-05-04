ef4471710ffd8a189513968cda2b9d3161aae43c
StevenIannucci <89707282+StevenIannucci@users.noreply.github.com>
2024-05-04 16:26:49 +0000

    May 4, 2024, 12:26 PM

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -41,19 +41,21 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('A random AMAZING idea:'),
-          BigCard(pair: pair),
-
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('A random AMAZING idea:'),
+            BigCard(pair: pair),
+        
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
       ),
     );
   }
