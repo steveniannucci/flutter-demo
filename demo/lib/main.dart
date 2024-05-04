51c792d651e54fa561ec189ed3f4007561c52d2a
StevenIannucci <89707282+StevenIannucci@users.noreply.github.com>
2024-05-04 16:54:54 +0000

    May 4, 2024, 12:54 PM

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -51,6 +51,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  var selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,9 +72,12 @@ class _MyHomePageState extends State<MyHomePage> {
                   label: Text('Favorites'),
                 ),
               ],
-              selectedIndex: 0,
+              selectedIndex: selectedIndex,
               onDestinationSelected: (value) {
-                print('selected: $value');
+                
+                setState(() {
+                  selectedIndex = value;
+                });
               },
             ),
           ),
