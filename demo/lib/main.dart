0c950ca4c4e0dd4b03b7b20a6d30ccfdc548fa6f
StevenIannucci <89707282+StevenIannucci@users.noreply.github.com>
2024-05-04 16:28:34 +0000

    May 4, 2024, 12:28 PM

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -46,7 +46,7 @@ class MyHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             BigCard(pair: pair),
-        
+            SizedBox(height: 10),
             ElevatedButton(
               onPressed: () {
                 appState.getNext();
